e16bc471e0ca0ab6de52e3f0f249fe51ff3ee8d7
Mohit Kumawat <mohitbheemainfotech@gmail.com>
2026-06-04 13:27:39 +0530

    fix signup api endpoint

--- frontend/src/pages/Auth/Signup.jsx
+++ frontend/src/pages/Auth/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import API from "../../api";
+import API from "../../api.js";
 
 const Signup = () => {
   const navigate = useNavigate();
